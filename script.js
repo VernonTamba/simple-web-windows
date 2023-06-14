da7376b28556363d08ae2d20525c2778cb0f3461
Vernon Tamba <vernon.tamba777@gmail.com>
2023-06-14 16:19:15 +0700

    Added the responsive design (somehow)

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ const taskbarIcon = document.querySelectorAll(".taskbar__icon");
 const appPreview = document.querySelector(".appPreview");
 const appPreviewImage = document.querySelector(".appPreview__image");
 const appPreviewHeader = document.querySelector(".appPreview__header");
+const taskbarMiddleIcons = document.querySelectorAll(".middle-icons");
+const previousButton = document.querySelector(
+  ".taskbar__middle-button.previous"
+);
+const nextButton = document.querySelector(".taskbar__middle-button.next");
 const months = [
   "January",
   "February",
@@ -80,39 +85,6 @@ taskbarIcon.forEach((icon) => {
   });
 });
 
-// Behavior of the taskbar icons when they are clicked (GSAP)
-// taskbarIcon.forEach((icon) => {
-//   icon.addEventListener("click", () => {
-//     iconsClicked.push(icon);
-//     if (
-//       appPreview.classList.contains("appPreview--display") &&
-//       appPreviewImage.getAttribute("src") === `./img/preview/${icon.alt}.jpg`
-//     ) {
-//       // appPreview.classList.remove("appPreview--display");
-//       icon.classList.remove("taskbar__icon--active");
-//       gsap.To(appPreview, {
-//         y: 0,
-//         duration: 5,
-//       });
-//       iconsClicked = [];
-//     } else {
-//       if (iconsClicked.length > 1) {
-//         icon.classList.remove("taskbar__icon--active");
-//         iconsClicked[0].classList.remove("taskbar__icon--active");
-//         iconsClicked.splice(0, 1);
-//       }
-//       // appPreview.classList.add("appPreview--display");
-//       gsap.To(appPreview, {
-//         y: 100,
-//         duration: 5,
-//       });
-//       icon.classList.add("taskbar__icon--active");
-//     }
-//     appPreviewHeader.textContent = icon.alt;
-//     appPreviewImage.src = `./img/preview/${icon.alt}.jpg`;
-//   });
-// });
-
 // GSAP for selected elements
 const timeline = gsap.timeline();
 
@@ -135,7 +107,62 @@ taskbarIcon.forEach((icon) => {
   );
 });
 
+// Previous and Next Buttons to navigate through taskbar icons
+let currentActiveTaskbarIcon = 0;
+let checkOnce = true;
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 1150) {
+    if (checkOnce) {
+      trackActiveTaskbarIconChanges();
+      checkActiveTaskbarIcon(currentActiveTaskbarIcon);
+      checkOnce = false;
+    }
+  } else {
+    previousButton.style.display = "none";
+    nextButton.style.display = "none";
+    taskbarMiddleIcons.forEach((icons) => {
+      icons.style.display = "block";
+    });
+    checkOnce = true;
+  }
+});
+
+previousButton.addEventListener("click", () => {
+  currentActiveTaskbarIcon--;
+  trackActiveTaskbarIconChanges();
+  checkActiveTaskbarIcon(currentActiveTaskbarIcon);
+});
+nextButton.addEventListener("click", () => {
+  currentActiveTaskbarIcon++;
+  trackActiveTaskbarIconChanges();
+  checkActiveTaskbarIcon(currentActiveTaskbarIcon);
+});
+
+const trackActiveTaskbarIconChanges = () => {
+  if (currentActiveTaskbarIcon === 0) {
+    previousButton.style.display = "none";
+    nextButton.style.display = "block";
+  } else if (currentActiveTaskbarIcon === taskbarMiddleIcons.length - 1) {
+    previousButton.style.display = "block";
+    nextButton.style.display = "none";
+  } else {
+    previousButton.style.display = "block";
+    nextButton.style.display = "block";
+  }
+};
+
+const checkActiveTaskbarIcon = (currentActive) => {
+  taskbarMiddleIcons.forEach((icon) => {
+    if (taskbarMiddleIcons[currentActive] === icon) {
+      icon.style.display = "block";
+    } else {
+      icon.style.display = "none";
+    }
+  });
+};
+
+// Notes for future learning purposes.
 // TODO: Try to use GSAP for the animation/transition of the app preview
 // TODO: Learn more about GSAP! And try to see add() and other stuff in the docs!
-// TODO: Add responsive design
 // TODO: Add page transition?
